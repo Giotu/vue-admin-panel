a2ae811031df3163ddb7aa37bd7edb10b840d00b
Giotu <evilalex1998@mail.ru>
2025-01-31 23:08:41 +0300

    fix: change some styles for component Ulink

--- src/components/ULink/ULink.vue
+++ src/components/ULink/ULink.vue
@@ -32,15 +32,37 @@ const linkClasses = computed(() => {
 </template>
 
 <style scoped lang="scss">
-@import '../../assets/styles/variables';
-
 .link {
+  display: inline-block;
+  position: relative;
+  font-weight: 600;
+  border-radius: 0.375rem;
   text-decoration: none;
-  color: inherit;
   transition: color 0.2s ease;
+  padding: 0.5rem;
+
+  &::after {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 100%;
+    width: 100%;
+    height: 2px;
+    transform: scaleX(0);
+    transform-origin: left;
+    transition: transform 0.2s ease;
+  }
+
+  &:hover::after {
+    transform: scaleX(1);
+  }
 
   &--primary {
     color: $color-primary;
+
+    &::after {
+      background-color: $color-primary;
+    }
 
     &:hover {
       color: $color-primary-hover;
@@ -49,6 +71,10 @@ const linkClasses = computed(() => {
 
   &--secondary {
     color: $color-secondary-link;
+
+    &::after {
+      background-color: $color-secondary-link;
+    }
 
     &:hover {
       color: $color-secondary-link-hover;
